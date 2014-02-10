ad3b3d7be81b082aab1bdf6902ece8943b88071d
Matt Wormley <mwormley@adobe.com>
2014-02-10 12:32:51 -0800

    argument added

--- scout/recipes/default.rb
+++ scout/recipes/default.rb
@@ -49,7 +49,7 @@ if node[:scout][:key]
   # schedule scout agent to run via cron
   cron "scout_run" do
     user node[:scout][:user]
-    command "#{scout_bin} #{node[:scout][:key]}#{name_attr}#{server_attr}#{roles_attr}#{http_proxy_attr}#{https_proxy_attr}#{environment_attr}"
+    command "#{scout_bin} #{node[:scout][:key]}#{name_attr}#{hostname_attr}#{server_attr}#{roles_attr}#{http_proxy_attr}#{https_proxy_attr}#{environment_attr}"
     only_if do File.exist?(scout_bin) end
   end
 else
